2962bb214a3e04965f31a26bf34e82996f307447
sameer dhulap <28100769+sameerdhulap@users.noreply.github.com>
2022-08-05 12:02:04 +0530

    strip version of code

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "WoosmapGeofencingCore",
+    platforms: [.iOS(.v11)],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "WoosmapGeofencingCore",
+            targets: ["WoosmapGeofencingCore"])
+    ],
+    dependencies: [
+        // Surge Package
+        .package(url: "https://github.com/Jounce/Surge.git", from: "2.3.0"),
+        // Realm
+        .package(url: "https://github.com/realm/realm-cocoa", from: "10.5.1")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "WoosmapGeofencingCore",
+            dependencies: ["Surge","RealmSwift"],
+            path: "Sources/WoosmapGeofencing"),
+        .testTarget(
+            name: "WoosmapGeofencingTests",
+            dependencies: ["WoosmapGeofencingCore"],
+            path: "Tests/WoosmapGeofencingTests")
+                    
+        
+    ]
+)
---
+++ README.md
@@ -0,0 +1,13 @@
+# Woosmap Geofencing SDK
+
+### Open Source SDKs to help developers collecting users’ locations in the right way.
+
+The Woosmap Geofencing SDK is a mobile cross-platform software development kit focused on gathering efficiently the users’ location, triggering events based on region monitoring, and providing categorized users’ zone of interest from geographical and temporal clusters.
+
+The SDK simplifies the integration of the location context in your mobile application by taking care of lower-level functionalities such as data collection or battery management.
+
+### Documentation
+
+All feature descriptions and guides to implement the Woosmap Geofencing iOS SDK are available on the [Woosmap developers documentation](https://developers.woosmap.com/products/geofencing-sdk/get-started/).
+
+If you are looking for the former documentation (before October 2021), it is still available in the [doc folder of the repository](https://github.com/woosmap/woosmap-geofencing-ios-sdk/tree/master/doc).
