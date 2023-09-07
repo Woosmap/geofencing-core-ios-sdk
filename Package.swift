742a4d75d4124248fa2f006967983ba6fdf2211f
sameer dhulap <28100769+sameerdhulap@users.noreply.github.com>
2023-09-07 15:20:04 +0530

    corrected swift package

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -17,12 +17,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "WoosmapGeofencingCore",
-            path: "Sources/WoosmapGeofencing"),
+            path: "Sources/WoosmapGeofencing", resources: [.process("Business Logic/Woosmap.xcdatamodeld")]),
         .testTarget(
             name: "WoosmapGeofencingTests",
             dependencies: ["WoosmapGeofencingCore"],
             path: "Tests/WoosmapGeofencingTests")
-                    
-        
     ]
 )
